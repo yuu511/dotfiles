87a8ae82c96d8d96a43a84a985c47523fa77c4e5
eli <rrrolento@yahoo.com>
2024-12-31 23:09:06 -0800

    Add tree to nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,22 @@
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- Relative line num
+vim.wo.relativenumber = true
+
+-- Disable netrw for nvim-tree 
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("config.lazy")
---
+++ nvim/lua/plugins/ripgrep.lua
@@ -0,0 +1,7 @@
+return {
+  {
+  "jremmen/vim-ripgrep",
+  config = function()
+  end,
+  }
+}
