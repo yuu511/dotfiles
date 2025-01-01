16db09054f6ccbdea30fed9364ad433194dd1ac1
eli <rrrolento@yahoo.com>
2025-01-01 10:09:50 -0800

    Fix syntax highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,21 @@
 return {
   {
    "nvim-treesitter/nvim-treesitter",
-   config=function()
+   build = ':TSUpdate',
+   opts = {
+     ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+     auto_install = true,
+     highlight = {
+       enable = true,
+       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+       --  If you are experiencing weird indenting issues, add the language to
+       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+       additional_vim_regex_highlighting = { 'ruby', 'markdown' },
+     },
+     indent = { enable = true, disable = { 'ruby' } },
+   },
+   config = function(_, opts)
+     require('nvim-treesitter.configs').setup(opts)
    end,
   }
 }
